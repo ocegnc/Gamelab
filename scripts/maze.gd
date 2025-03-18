d00597d42f89ad9b4d8943aa2b1e74c0b1be4730
ocegnc <oceane.guennec@gmail.com>
2025-03-18 13:05:54 +0100

    maze

---
+++ scripts/maze.gd
@@ -0,0 +1,34 @@
+
+extends TileMap
+
+const TILE_LAYER = 0  # Layer du TileMap (0 par défaut)
+#const GRID_SIZE = 2 #2*2 grid
+
+# Liste des indices des tuiles principales dans le TileSet
+const TILE_SET_INDICES = [0, 1]  # Ajuste selon ton TileSet
+
+func _ready():
+	randomize()  # Assure un tirage aléatoire différent à chaque exécution
+	place_random_tile()
+
+# Fonction pour placer une tuile aléatoire au centre de l'écran
+func place_random_tile():
+	#var random_source = TILE_SET_INDICES[randi_range(0, TILE_SET_INDICES.size() - 1)]  # Choisir une tuile au hasard
+	var random_tileX = randi_range(0, 1) #choix au hasard d'une tuiles de la tiles (CHANGE BY 2)
+	
+	var tile_size = Vector2(16, 16)  # Taille de chaque tuile en pixels (à ajuster selon ton TileSet)
+# Position désirée en pixels (ex: placer l'origine à 100px, 200px de l'écran)
+	var desired_pixel_position = Vector2(200, 200)
+	# Convertir la position en pixels en coordonnées de TileMap
+	var grid_position = Vector2i(desired_pixel_position / tile_size)
+	
+	set_cell(TILE_LAYER, grid_position, 0, Vector2i(1, 0))  # Placer la tuile au centre
+	set_cell(TILE_LAYER, grid_position+Vector2i(16,0), 1, Vector2i(2, 0))
+	
+	# WHEN WE WILL HAVE MORE TILES
+	#var num_source = 0
+	#for i in TILE_SET_INDICES:
+		#set_cell(TILE_LAYER, center_pos, num_source, Vector2i(1, 0)) 
+		#num_source+=1
+#
+	
