fd395e9b393940a32120d266053bf17fa7787fe6
Marius LEGOUALLEC <mlegoual@gmail.com>
2025-05-20 04:36:27 +0200

    Timer + score working

--- scripts/baguette.gd
+++ scripts/baguette.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-
+@onready var GameState = preload("res://scripts/game_state.gd")
 # Variables de mouvement
 var current_speed = 50.0
 const BASE_SPEED = 50.0
@@ -99,9 +99,10 @@ func reset_after_knockback():
 	knockback_velocity = Vector2.ZERO
 
 # Fonction à appeler quand la baguette ramasse un aliment
+
 func aliment_ramasse():
-	aliments_ramasses += 1
-	print(aliments_ramasses)
+	aliments_ramasses += 1                          
+	GameState.instance.add_score(1)
+	
 	if aliments_ramasses == 6:
 		get_tree().change_scene_to_file("res://scenes/win_screen.tscn")
-		print("win")
